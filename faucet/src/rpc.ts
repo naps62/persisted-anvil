e92bf75241bb5569d848734089dbfe954dfe92f7
Miguel Palhas <mpalhas@gmail.com>
2024-10-10 16:55:52 +0100

    code review

--- faucet/src/rpc.ts
+++ faucet/src/rpc.ts
@@ -4,6 +4,6 @@ import { anvil } from "viem/chains";
 const subvisual = { ...anvil, id: 7566690 };
 
 export const rpc = createPublicClient({
-	chain: subvisual,
-	transport: http("http://anvil:8545"),
+  chain: subvisual,
+  transport: http(process.env.ANVIL_URL),
 }).extend(walletActions);
